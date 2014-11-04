a0c0bb77a2671bd179efef678fad225a50ce5a4d
Jabez <wjabezkingsly@gmail.com>
2014-11-03 19:44:36 -0500

    Pretty Colors + UI

--- wordsearch.cpp
+++ wordsearch.cpp
@@ -1,29 +1,143 @@
 #include "wordsearch.h"
+#include <Windows.h>
 #include <fstream>
 #include <iostream>
 #include <ctime>
 
 
 void WordSearch::display(){
+	std::cout << "   ";
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);///For making pretty colors
+	if (hStdout == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "Error while getting input handle" << std::endl;
+	}
+	//sets color to red on black
+	SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_INTENSITY);	
+
+	for (int i = 0; i < PUZZLE_SIZE; i++)
+		std::cout << char(i + 65) << " ";
+	std::cout << "\n\n";
+
+	//white on black
+	SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 	for (int i = 0; i < PUZZLE_SIZE; i++){
+		SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_INTENSITY);//red on black
+		std::cout << i << "  ";
+		SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);//white on black
 		for (int j = 0; j < PUZZLE_SIZE; j++)
+		{
+			if (found[i][j])
+				SetConsoleTextAttribute(hStdout, FOREGROUND_GREEN | FOREGROUND_INTENSITY); // green
 			std::cout << puzzle[i][j] << " ";
+			SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);//white on black
+		}
 		std::cout << "\n";
 	}
+	SetConsoleTextAttribute(hStdout, FOREGROUND_GREEN | FOREGROUND_BLUE);//blue-green
+	std::cout << "\n";
+	for (int i = 0; i < words.size(); i++)
+		if (!words[i]->found)
+			std::cout  << words[i]->word << "\n";
+			//std::cout << words[i]->word << "\t\t" << words[i]->start.col << words[i]->start.row << " " << words[i]->end.col << words[i]->end.row  << "\n";
+	SetConsoleTextAttribute(hStdout, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);//white
 	return;
 }
 
-void WordSearch::solve(){
-	display();
+void WordSearch::discover(wordx a){
+	int hs = 0;
+	int vs = 0;
+	if (a.start.col > a.end.col)
+		hs = -1;
+	else if (a.start.col < a.end.col)
+		hs = 1;
+	if (a.start.row > a.end.row)
+		vs = -1;
+	else if (a.start.row < a.end.row)
+		vs = 1;
+	int hindex = a.start.col - 65;
+	int vindex = a.start.row - 48;
+	found[vindex][hindex] = true;
+	do{
+		vindex += vs;
+		hindex += hs;
+		found[vindex][hindex] = true;		
+	} while (hindex != a.end.col - 65 || vindex != a.end.row - 48);
+}
+
+bool WordSearch::check(std::string answer)
+{
+	if (answer.length() != 5)
+		return false;
+	char column1, column2, row1, row2;
+	column1 = answer[0];
+	column2 = answer[3];
+	row1 = answer[1];
+	row2 = answer[4];
 	for (int i = 0; i < words.size(); i++)
-		std::cout << "\n" << words[i]->start.col << words[i]->start.row << ": " << words[i]->end.col << words[i]->end.row << "\t\t" << words[i]->word;
-	return;
+	{
+		if (answer[0] == words[i]->start.col & answer[3] == words[i]->end.col & answer[1] == words[i]->start.row & answer[4] == words[i]->end.row){
+			words[i]->found = true;
+			discover(*(words[i]));
+			return true;
+		}
+	}
+	return false;
+}
+
+int WordSearch::words_left()
+{
+	int x= 0;
+	for (int i = 0; i < words.size(); i++)
+		if (words[i]->found == false)
+			x++;
+	return x;
+}
+
+int WordSearch::words_found()
+{
+	int x = 0;
+	for (int i = 0; i < words.size(); i++)
+		if (words[i]->found == true)
+			x++;
+	return x;
+}
+
+void WordSearch::solve(){
+	while (1)
+	{
+		display();
+		std::string answer;
+		std::cout << "Enter the range (ex. A1:A9 for horizontal) you believe is an answer: ";
+		getline(std::cin, answer);
+		std::cout << std::endl;
+		for (int i = 0; i < 3; i++) {
+			std::cout << ". "; 
+			_sleep(300);
+		}
+		if (check(answer)){
+			std::cout << "YOU FOUND A WORD!!!! :) \n\n";
+		}
+		else{
+			std::cout << "Sorry that's not a word :(\n\n";
+		}
+		if (words_left() == 0)
+		{
+			std::cout << "Woohoo! You Finished the Puzzle!!!!\n\n\n";
+			break;
+		}
+		_sleep(1000);
+	}
 }
 
 void WordSearch::clear(){
 	for (int i = 0; i < PUZZLE_SIZE; i++)
 		for (int j = 0; j < PUZZLE_SIZE; j++)
+		{
 			puzzle[i][j] = '\0';	//set all locations to NUL
+			found[i][j] = false;
+		}
 	for (int i = 0; i < words.size(); i++)
 		delete words[i];
 	words.clear();
@@ -34,7 +148,7 @@ void WordSearch::generate(){
 	//Horizontal Directions: -1 for left, 1 for right
 	//Vertical Directions: -1 for down, 1 for up
 	wordx* word;
-	for (int i = 0; i < 500; i++){		//if you can't find a word that fits in 10,000 attempts, stop
+	for (int i = 0; i < 500; i++){		//if you can't find a word that fits in 500 attempts, stop
 		int dh, dv;
 		do{
 			dh = rand() % 3 - 1;
@@ -55,7 +169,7 @@ void WordSearch::generate(){
 	for (int i = 0; i < PUZZLE_SIZE; i++)
 		for (int j = 0; j < PUZZLE_SIZE; j++)
 			if (puzzle[i][j] == '\0')
-				puzzle[i][j] = '_';//(rand() % 26) + 97;
+				puzzle[i][j] = (rand() % 26) + 97;
 	return;
 }
 
@@ -78,9 +192,9 @@ wordx* WordSearch::testWord(std::string word, int x, int y, int v, int h){
 	wordx* baby = new wordx;
 	baby->found = false;
 	baby->word = word;
-	baby->start.row = x;
+	baby->start.row = x + 48;
 	baby->start.col = y + 65;
-	baby->end.row = x + v*i;
+	baby->end.row = x + v*i + 48;
 	baby->end.col = y + h*i + 65;
 	for (i = 0; word[i] != '\0'; i++)
 		puzzle[x + v*i][y + h*i] = word[i];//write word into puzzle
@@ -102,7 +216,8 @@ Dictionary::Dictionary(){
 	std::string x;
 	while (fin){
 		fin >> x;
-		load( x.c_str() , &root);//Load all words in dictionary file into the dictionary
+		if (x.length() > 4)
+			load( x.c_str() , &root);//Load all words in dictionary file into the dictionary
 	}
 	fin.close();
 }
--- wordsearch.h
+++ wordsearch.h
@@ -1,10 +1,10 @@
 #include <vector>
 #include <string>
-#define FILE "../WordSearch/dictionary.txt"
-#define ALPHABET 26						//defines number of chars in alphabet
-#define PUZZLE_SIZE 10					//defines size of puzzle (all are square)
-#define BOUND_LOWER 0					//defines lowest  possible index of location in puzzle
-#define BOUND_UPPER PUZZLE_SIZE - 1		//defines highest possible index of location in puzzle
+#define FILE "../WordSearch/dictionary.txt"		//defines dictionary file
+#define ALPHABET 26								//defines number of chars in alphabet
+#define PUZZLE_SIZE 10							//defines size of puzzle (all are square)
+#define BOUND_LOWER 0							//defines lowest  possible index of location in puzzle
+#define BOUND_UPPER PUZZLE_SIZE - 1				//defines highest possible index of location in puzzle
 
 struct trie{
 	bool is_word;
@@ -24,7 +24,7 @@ public:
 
 struct point{
 	char col;	//column
-	int row;	//row
+	char row;	//row
 };
 
 struct wordx{//stores start point, end point, word
@@ -36,16 +36,22 @@ struct wordx{//stores start point, end point, word
 
 class WordSearch{
 	char puzzle[PUZZLE_SIZE][PUZZLE_SIZE];
+	bool  found[PUZZLE_SIZE][PUZZLE_SIZE];
 	std::vector<wordx*> words;	//vector of words and their locations in the puzzle
 	Dictionary dict;
 	wordx* testWord(std::string,int,int,int,int);	
 	/*checks if word can be stored with given string, x coordinate, y coordinate, direction
 	if possible write word into puzzle and return pointer to word, else return NULL*/
-	void display();		//displays the Puzzle, called from solve
-	bool exists(std::string);//returns true if word has been used in puzzle already
+	void display();				//displays the Puzzle, called from solve
+	bool exists(std::string);	//returns true if word has been used in puzzle already
+	bool check(std::string);
+	int words_left();
+	int words_found();
+	void discover(wordx);	//sets bools in found array to true upon discovery of a word
 public:
 	~WordSearch();
 	void generate();	//generate new puzzle with random words
 	void solve();		//User Interface for solving the Word Search
 	void clear();		//clear the puzzle
+	int stats();
 };
